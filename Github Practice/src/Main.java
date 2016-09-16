eb238cfb2189aabee194361ca1a867d67a00e754
Andrae <wheeyhalo@yahoo.com>
2016-09-16 09:36:52 -0400

    Initial checking of sales tools

---
+++ Github Practice/src/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+		
+	}
+	
+	public static void displayGreeting() {
+		
+		System.out.println("Hello happy sales people");
+		System.out.println("This app shows sales data");
+	}
+}
